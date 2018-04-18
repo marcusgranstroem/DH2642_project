781c721d846f9fedf238b6179c1ff8451ad4b48d
Marcus Granström <mgranst@kth.se>
2018-04-18 19:23:52 +0200

    Typecasting date.

--- src/actions.js
+++ src/actions.js
@@ -29,22 +29,30 @@ export function fetchEarthquakes(start=0, end=0, magnitude=0) {
 	// Limit amount of return values
 	var limit = 200;
 	dispatch(requestEarthquakes());
-
+	
 	if(magnitude < 0) {
 	    console.error("Magnitude was set to" + magnitude);
 	    magnitude = 0;
 	}
-	
 	// API only handles timeformat in ISO8601
-	if(start === 0 || end === 0) {
-	    if(start === 0) {
-		let d = new Date((new Date().valueOf() - 1000*3600*24));
-		start = d.toISOString(); 
-	    }
-	    if(end === 0) {
-		let d = new Date();
-		end = d.toISOString();
-	    }
+	if(start !== 0 && start !=="") {
+	    let d = new Date(start);
+	    start = d.toISOString();
+	    console.log(start);
+	} else {
+	    let d = new Date((new Date().valueOf() - 1000*3600*24));
+	    start = d.toISOString(); 
+	    console.log(start);
+	}
+	if(end !== 0 && end !=="") {
+	    let e = new Date(end);
+	    end = e.toISOString();
+	    console.log(end);
+	    
+	} else {
+	    let d = new Date();
+	    end = d.toISOString();
+	    console.log(end);
 	}
 	
 	return fetch(`https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${start}&endtime=${end}&minmagnitude=${magnitude}&limit=${limit}`)
