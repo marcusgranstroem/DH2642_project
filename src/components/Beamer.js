8fc3712ff4401a834cef50e942f0e0274dc49690
Marcus Granström <mgranst@kth.se>
2018-04-25 14:20:38 +0200

    Merge.

--- src/components/Beamer.js
+++ src/components/Beamer.js
@@ -1,29 +1,72 @@
 import logo from '../logo.svg';
-import EarthquakeInfoConainer from '../containers/EarthquakeInfoContainer.js';
+//import EarthquakeInfo from '../components/EarthquakeInfo.js';
 import React from 'react';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
+import Popover, {PopoverAnimationVertical} from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
 
-/* Style is used to set coordinate alignement from center of the object,
+/* 
+ * Style is used to set coordinate alignement from center of the object,
  * instead of the top left corner.
+ * This component holds a state to provide info whether the earthquake info
+ * is open or closed.
  */
 
-const Beamer = ({lat, lng, magnitude}) => (
-  <div className="Beamer">
-    <img src={logo} className="Beamer-sign" alt="beamer" style={{
-      position: 'absolute',
-      width: magnitude,
-      height: magnitude,
-      left: -magnitude / 2,
-      top: -magnitude / 2}}
-    />
-    <EarthquakeInfoConainer/>
-  </div>
-);
+export default class Beamer extends React.Component {
 
-Beamer.propTypes = {
-  lat: PropTypes.number.isRequired,
-  lng: PropTypes.number.isRequired,
-  magnitude: PropTypes.number.isRequired
-};
+    constructor(props) {
+    super(props);
 
-export default Beamer;
+    this.state = {
+      open: false
+    };  
+  }
+    
+    handleClick = (event) => {
+	// This prevents ghost click.
+	event.preventDefault();
+	
+	this.setState({
+	    open: true,
+	    anchorEl: event.currentTarget
+	});
+    };
+
+    handleRequestClose = () => {
+	this.setState({
+	    open: false
+	});
+    };
+    
+    render() {
+	return (
+	    <div className="Beamer">
+	      <img src={logo} onClick={this.handleClick} className="Beamer-sign" alt="beamer" style={{
+		       position: 'absolute',
+		       width: this.props.magnitude,
+		       height: this.props.magnitude,
+		       left: -this.props.magnitude / 2,
+		   top: -this.props.magnitude / 2}}
+		   />
+              <Popover
+		open={this.state.open}
+		anchorEl={this.state.anchorEl}
+		anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+		targetOrigin={{horizontal: 'left', vertical: 'top'}}
+		onRequestClose={this.handleRequestClose}
+		animation={PopoverAnimationVertical}
+		>
+		<Menu>
+		  <MenuItem primaryText="Refresh" />
+		  <MenuItem primaryText="Help &amp; feedback" />
+		  <MenuItem primaryText="Settings" />
+		  <MenuItem primaryText="Sign out" />
+		</Menu>
+              </Popover>
+
+	    </div>
+	);
+    }
+}
+ 
--- src/components/EarthquakeInfo.js
+++ src/components/EarthquakeInfo.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import Popover, {PopoverAnimationVertical} from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
 
-const EarthquakeInfo = ({name, magnitude, lat, lon, date}) => (
-    <div className="Earthquake-info">
-      <p>Name: {name}</p>
-      <p>magnitude: {magnitude}</p>
-      <p>Latitude: {lat}</p>
-      <p>Longitude: {lon}</p>
-      <p>Date: {date}</p>
-    </div>
-);
+export default class EarthquakeInfo extends React.Component {
 
-export default EarthquakeInfo;
+    render() {
+	return (
+	    <div>
+              <RaisedButton
+		onClick={this.handleClick}
+		label="Click me"
+		/>
+              <Popover
+		open={this.state.open}
+		anchorEl={this.state.anchorEl}
+		anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+		targetOrigin={{horizontal: 'left', vertical: 'top'}}
+		onRequestClose={this.handleRequestClose}
+		animation={PopoverAnimationVertical}
+		>
+		<Menu>
+		  <MenuItem primaryText="Refresh" />
+		  <MenuItem primaryText="Help &amp; feedback" />
+		  <MenuItem primaryText="Settings" />
+		  <MenuItem primaryText="Sign out" />
+		</Menu>
+              </Popover>
+	    </div>
+	);
+    }    
+}
+
