8e399d3a2dfd9361d4555781715bc0b624ce1fae
Nackey <bhatnathan@gmail.com>
2018-04-25 16:04:49 +0200

    Made the list into menuItems

--- src/components/EarthquakeInfo.js
+++ src/components/EarthquakeInfo.js
@@ -1,14 +1,15 @@
 import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
 import React from 'react';
 
 const EarthquakeInfo = ({name, lat, long, mag, date }) => (
     <div className="Earthquake-info">
       <Menu>
-	<p> Place: {name} </p>
-	<p> Latitude: {lat} </p>
-	<p> Longitude: {long} </p>
-	<p> Magnitude: {mag} </p>
-	<p> Time: {date} </p>
+    	<MenuItem primaryText={'Place: ' + name} disabled={true}/>
+        <MenuItem primaryText={'Latitude: ' + lat} disabled={true}/>
+        <MenuItem primaryText={'Longitude: ' + long} disabled={true}/>
+        <MenuItem primaryText={'Magnitude: ' + mag} disabled={true}/>
+        <MenuItem primaryText={'Time: ' + date} disabled={true}/>
       </Menu>
     </div>
 );
