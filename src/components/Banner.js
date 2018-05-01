dba85aa78b823310a692a28e09861a482aa5edde
Nackey <bhatnathan@gmail.com>
2018-05-01 16:06:33 +0200

    Added Drawer for search. fixes: #4

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,31 +1,71 @@
 import React from 'react';
 import logo from '../logo.svg';
-import { Navbar, FormGroup, FormControl } from 'react-bootstrap';
+import Drawer from 'material-ui/Drawer';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import DatePicker from 'material-ui/DatePicker';
+import TextField from 'material-ui/TextField'
+import MaterialIcon from 'material-icons-react';
 
-const Banner = ({fetchEarthquakes}) => (
-<header className="App-header">
-    <h2 className="Developed-by">Developed by Nathan Bhat <br />& Marcus Granström</h2>
-    <div className="App-logotitle">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">QuakeSee</h1>
-    </div>
-    <Navbar.Form pullRight>
-      <FormGroup className="Search-bar">
-        <label>
-          Minimum Magnitude:
-          <FormControl inputRef={(ref) => {this.magnitude = ref}} type="number" placeholder="Mag" onChange={() => fetchEarthquakes(this.starttime.value , this.endtime.value, this.magnitude.value)}/>
-        </label>
-        <label>
-          Start Time:
-          <FormControl inputRef={(ref) => {this.starttime = ref}} type="date" onChange={() => fetchEarthquakes(this.starttime.value , this.endtime.value, this.magnitude.value)}/>
-        </label>
-        <label>
-          End Time:
-          <FormControl inputRef={(ref) => {this.endtime = ref}} type="date" onChange={() => fetchEarthquakes(this.starttime.value , this.endtime.value, this.magnitude.value)}/>
-        </label>
-      </FormGroup>{' '}
-    </Navbar.Form>
-</header>
-);
+export default class Banner extends React.Component {
 
-export default Banner;
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false,
+            mag: 0,
+            minDate: '',
+            maxDate: ''
+        };
+    }
+
+    handleToggle = () => this.setState({open: !this.state.open});
+
+    _handleTextFieldChange = (e) => {
+        this.setState({
+            mag: e.target.value
+        });
+    }
+
+    _handleMinDateChange = (e, date) => {
+        this.setState({
+            minDate: date
+        });
+    }
+
+    _handleMaxDateChange = (e, date) => {
+        this.setState({
+            maxDate: date
+        });
+    }
+
+    render () {
+        return (
+            <header className="App-header">
+                <h2 className="Developed-by">Developed by Nathan Bhat <br />& Marcus Granström</h2>
+                <div className="App-logotitle">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <h1 className="App-title">QuakeSee</h1>
+                </div>
+                <div>
+                    <FloatingActionButton mini={true} onClick={this.handleToggle}>
+                        <MaterialIcon icon="search"/>
+                    </FloatingActionButton>
+                    <Drawer docked={false} width={300} open={this.state.open} openSecondary={true} onRequestChange={(open => this.setState({open}))}>
+                        <TextField type="number" hintText="Minimum Magnitude" onChange={
+                            (e) => {this._handleTextFieldChange(e);
+                                this.props.fetchEarthquakes(this.state.minDate, this.state.maxDate, e.target.value);}
+                        }/>
+                        <DatePicker hintText="Minimum Date" onChange={
+                            (e, date) => {this._handleMinDateChange(e, date);
+                                this.props.fetchEarthquakes(date, this.state.maxDate, this.state.mag);}
+                        }/>
+                        <DatePicker hintText="Maximum Date" onChange={
+                            (e, date) => {this._handleMaxDateChange(e, date);
+                                this.props.fetchEarthquakes(this.state.minDate, date, this.state.mag);}
+                        }/>
+                    </Drawer>
+                </div>
+            </header>
+        );
+    }
+}
