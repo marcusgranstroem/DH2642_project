1fb3976a1f4ec94144968b3731c609d6cd948334
Marcus Granström <mgranst@kth.se>
2018-05-02 14:21:51 +0200

    Removed merge conflicts.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import BannerContainer from './containers/BannerContainer.js';
-<<<<<<< HEAD
 import MapContainer from './containers/MapContainer.js';
-=======
 import SearchSnackContainer from './containers/SearchSnackContainer.js';
->>>>>>> 096bcdeb4e6e0ab1bb5ba1b52f0f5ea955f3174b
 import React, { Component } from 'react';
 import './css/App.css';
 
