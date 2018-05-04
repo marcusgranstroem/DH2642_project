5cdeffacf651cbe978aab363a7cbbe8ebe151ddd
Marcus Granström <mgranst@kth.se>
2018-05-04 11:53:08 +0200

    Mostly formatting.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -20,59 +20,86 @@ export default class Banner extends React.Component {
             maxDate: ''
         };
     }
-
+    
     handleToggle = () => this.setState({open: !this.state.open});
-
+    
     _handleTextFieldChange = (e) => {
         this.setState({
             mag: e.target.value
         });
     }
-
+    
     _handleMinDateChange = (e, date) => {
         this.setState({
             minDate: date
         });
     }
-
+    
     _handleMaxDateChange = (e, date) => {
         this.setState({
             maxDate: date
         });
     }
-
+    
     render () {
         var loader;
         if (this.props.searching)
             loader = (<CircularProgress/>);
         return (
             <header className="App-header">
-                <h2 className="Developed-by">Developed by Nathan Bhat <br />& Marcus Granström</h2>
-                <div className="App-logotitle">
-                  <img src={logo} className="App-logo" alt="logo" />
-                  <h1 className="App-title">QuakeSee</h1>
-                </div>
-                <div className="End-bar">
-		            <Login/>
-                    <FloatingActionButton mini={true} onClick={this.handleToggle} className="Search-button">
-                        <MaterialIcon icon="search"/>
-                    </FloatingActionButton>
-                    <Drawer docked={false} width={300} open={this.state.open} openSecondary={true} onRequestChange={(open => this.setState({open}))}>
-                        <TextField type="number" hintText="Minimum Magnitude" onChange={
-                            (e) => {this._handleTextFieldChange(e);
-                                this.props.fetchEarthquakes(this.state.minDate, this.state.maxDate, e.target.value);}
-                        }/>
-                        <DatePicker hintText="Minimum Date" onChange={
-                            (e, date) => {this._handleMinDateChange(e, date);
-                                this.props.fetchEarthquakes(date, this.state.maxDate, this.state.mag);}
-                        }/>
-                        <DatePicker hintText="Maximum Date" onChange={
-                            (e, date) => {this._handleMaxDateChange(e, date);
-                                this.props.fetchEarthquakes(this.state.minDate, date, this.state.mag);}
-                        }/>
-                        {loader}
-                    </Drawer>
-                </div>
+              <h2 className="Developed-by">
+                Developed by Nathan Bhat <br/>
+                & Marcus Granström
+              </h2>
+              <div className="App-logotitle">
+                <img src={logo}
+                     className="App-logo"
+                     alt="logo"/>
+                <h1 className="App-title">
+                  QuakeSee
+                </h1>
+              </div>
+              <div className="End-bar">
+		<Login/>
+                <FloatingActionButton mini={true}
+                                      onClick={this.handleToggle}
+                                      className="Search-button">
+                  <MaterialIcon icon="search"/>
+                </FloatingActionButton>
+                <Drawer docked={false}
+                        width={300}
+                        open={this.state.open}
+                        openSecondary={true}
+                        onRequestChange={(open => this.setState({open}))
+                  }>
+                <TextField type="number"
+                        hintText="Minimum Magnitude"
+                        onChange={(e) => {
+                            this._handleTextFieldChange(e);
+                            this.props.fetchEarthquakes(
+                                this.state.minDate,
+                                this.state.maxDate,
+                                e.target.value);}
+                  }/>
+                <DatePicker hintText="Minimum Date"
+                        onChange={(e, date) => {
+                            this._handleMinDateChange(e, date);
+                            this.props.fetchEarthquakes(
+                                date,
+                                this.state.maxDate,
+                                this.state.mag);}
+                  }/>
+                <DatePicker hintText="Maximum Date"
+                        onChange={(e, date) => {
+                            this._handleMaxDateChange(e, date);
+                            this.props.fetchEarthquakes(
+                                this.state.minDate,
+                                date,
+                                this.state.mag);}
+                  }/>
+                  {loader}
+                </Drawer>
+              </div>
             </header>
         );
     }
--- src/components/Reports.js
+++ src/components/Reports.js
@@ -9,77 +9,78 @@ import PropTypes from 'prop-types';
 const uuidv1 = require('uuid/v1');
 
 export default class Reports extends React.Component {
+    
+    state = {
+        writing: false
+    };
 
-  state = {
-    writing: false
-  };
+    handleWriting = () => {
+        this.setState({writing: true});
+    };
+    
+    handleSubmit = (text) => {
+        console.log(text);
+        this.setState({writing: false});
+    };
+    
+    render() {
+        const actions = [
+            <FlatButton label="Close"
+                        secondary={true}
+                        onClick={this.props.close}
+                        />,
+            <FlatButton label="Write New"
+                        primary={true}
+                        onClick={this.handleWriting}
+	                disabled={this.state.writing}
+                        />,
+        ];
+        
+        const reports = [];
 
-  handleWriting = () => {
-    this.setState({writing: true});
-  };
+        if (!this.props.reports) //Check if there are no Reports
+            reports.push(<p>No user reports yet.</p>);
+        else {
+            // Get inside first object which is "0"
+            let v = this.props.reports[0];
 
-  handleSubmit = (text) => {
-      console.log(text);
-      this.setState({writing: false});
-  };
+            // Loop trough all reports for current earthquake 
+            for(var key in v)
+                if(v.hasOwnProperty(key)) {
+                    reports.push(
+                        <div key={uuidv1()}>
+                          <ListItem primaryText={v[key]['nickName']}
+                                    secondaryText={<p>{v[key]['comment']}</p>}
+                                    disabled={true}/>
+                          <Divider/>
+                        </div>
+                    );
+                }
+        } 
 
-  render() {
-    const actions = [
-      <FlatButton
-        label="Close"
-        secondary={true}
-        onClick={this.props.close}
-      />,
-      <FlatButton
-        label="Write New"
-        primary={true}
-        onClick={this.handleWriting}
-	disabled={this.state.writing}
-      />,
-    ];
-      
-      const reports = [];
-      if (!this.props.reports) //Check if there are no Reports
-          reports.push(<p>No user reports yet.</p>);
-      else {
-          // Get inside first object which is "0"
-          let tmp = this.props.reports[0];
-          for(var key in tmp) {
-              if(tmp.hasOwnProperty(key)) {
-                  reports.push(
-                      <div key={uuidv1()}>
-                        <ListItem primaryText={tmp[key]['nickName']} secondaryText={<p>{tmp[key]['comment']}</p>} disabled={true}/>
-                        <Divider />
-                      </div>
-                  );
-              }
-          }
-          
-      } 
-      /*     TODO
-             if (this.state.writing)
-             reports.push(
-             <div>
-               <NewReport userName={"Marcus Granström"} submitNew={this.handleSubmit}/>
-               <Divider />
-             </div>
-             );*/
-      
-      
-    return (
-        <Dialog
-          title="User Reports"
-          actions={actions}
-          modal={true}
-          open={this.props.open}
-          autoScrollBodyContent={true}
-          >
-          <List>
-            {reports}
-          </List>
-        </Dialog>
-    );
-  }
+        if (this.state.writing)
+            reports.push(
+                <div>
+                  <NewReport userName={"Marcus Granström"}
+                             submitNew={this.handleSubmit}/>
+                  <Divider/>
+                </div>
+            );
+        
+        
+        return (
+            <Dialog title="User Reports"
+                    actions={actions}
+                    modal={true}
+                    open={this.props.open}
+                    autoScrollBodyContent={true}
+                    >
+              <List>
+                {reports}
+              </List>
+            </Dialog>
+        );
+    }
 }
 
 Reports.propTypes = {
