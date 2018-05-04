05344245da57e024bf49ec37df09e6b9b83d2409
Marcus Granström <mgranst@kth.se>
2018-05-04 11:53:08 +0200

    Mostly formatting.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import App from './App';
 import 'babel-polyfill';
+import createDebounce from 'redux-debounced';
 import { createLogger } from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
-import quakeSeeReducer from './reducers';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import { fetchEarthquakes } from './actions';
-import './index.css';
+import firebase from './firebase';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Provider } from 'react-redux';
+import quakeSeeReducer from './reducers';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import thunkMiddleware from 'redux-thunk';
-import createDebounce from 'redux-debounced';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-import firebase from './firebase';
+
+import './index.css';
 
 const loggerMiddleware = createLogger();
 
