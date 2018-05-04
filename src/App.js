1cd5adc015f0157fa35318e36436cdf54963fb4e
Marcus Granström <mgranst@kth.se>
2018-05-04 11:53:08 +0200

    Mostly formatting.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import BannerContainer from './containers/BannerContainer.js';
+import firebase from './firebase';
 import MapContainer from './containers/MapContainer.js';
-import SearchSnackContainer from './containers/SearchSnackContainer.js';
 import React, { Component } from 'react';
+import SearchSnackContainer from './containers/SearchSnackContainer.js';
+
 import './css/App.css';
 
 class App extends Component {
-
+    
     render() {
 	return (
 	  <div className="App">
 		<BannerContainer/>
 		<MapContainer/>
-        <SearchSnackContainer/>
+                <SearchSnackContainer/>
 	  </div>
 	);
-  }
+    }
 }
 
 export default App;
